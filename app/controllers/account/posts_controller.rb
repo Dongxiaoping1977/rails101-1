be06e06c252363a5b865c5733585511ef5e2743d
dongxiaoping <dongxiaoping1977@gmail.com>
2017-01-04 10:15:50 +0800

    implement my groups

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,4 +1,4 @@
-class Account::GroupsController < ApplicationController
+class Account::PostsController < ApplicationController
   before_action :authenticate_user!
   def index
     @posts = current_user.posts
